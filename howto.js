6e97b78a63307964823252066f5e84442c59f23b
evgeny <ekarpovsky@gmail.com>
2025-07-25 07:41:24 +0000

    remove unusable files

--- howto.js
+++
@@ -1,9 +0,0 @@
-const howtoBtn = document.getElementById('howto-toggle');
-const howtoSection = document.getElementById('howto-section');
-
-howtoBtn.addEventListener('click', () => {
-  howtoSection.classList.toggle('expanded');
-  howtoBtn.textContent = howtoSection.classList.contains('expanded') 
-    ? 'Hide How To Start?' 
-    : 'How To Start?';
-});
--- intro.js
+++
@@ -1,9 +0,0 @@
-const introBtn = document.getElementById('intro-toggle');
-const introSection = document.getElementById('intro-section');
-
-introBtn.addEventListener('click', () => {
-  introSection.classList.toggle('expanded');
-  introBtn.textContent = introSection.classList.contains('expanded') 
-    ? 'Hide SamApp Intro' 
-    : 'What is SamApp?';
-});
